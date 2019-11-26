f226424d46c10ba66c0896c74d1463a897798bd3
Rushiraj <rushibrahmbhatt33@gmail.com>
2019-11-26 18:39:22 +0530

    Updates

---
+++ precache-manifest.c68beedf4b5d6fee4903162a87b4bac7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9177556ac4f8c1e929469074abda0f5c",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "b86b77cfaa1a0b6d0d5d",
+    "url": "/todo/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "69f816d13545fc66a5e7",
+    "url": "/todo/static/css/main.4cb08038.chunk.css"
+  },
+  {
+    "revision": "b86b77cfaa1a0b6d0d5d",
+    "url": "/todo/static/js/2.b54a2827.chunk.js"
+  },
+  {
+    "revision": "69f816d13545fc66a5e7",
+    "url": "/todo/static/js/main.3ed5bce5.chunk.js"
+  },
+  {
+    "revision": "6662a8325481229d10f5",
+    "url": "/todo/static/js/runtime-main.18821b97.js"
+  }
+]);
